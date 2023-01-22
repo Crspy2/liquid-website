0362739cfd989874ecd43532d39131bd42872365
Crspy <saltycactus2006@gmail.com>
2023-01-21 21:32:47 -0800

    Added Social

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -58,16 +58,16 @@ const Footer = (props: Props) => {
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" className="hover:underline">Liquid</Link>. All Rights Reserved.
         </span>
         <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-            <Link href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <Link href="https://discord.gg/H4SZNkEKex" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <FaDiscord />
             </Link>
-            <Link href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <Link href="https://youtube.com/@liquidcx" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <FaYoutube />
             </Link>
-            <Link href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <Link href="https://twitter.com/liquid_cx" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <FaTwitter />
             </Link>
-            <Link href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <Link href="https://www.tiktok.com/@liquid_cx" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <FaTiktok />
             </Link>
         </div>
