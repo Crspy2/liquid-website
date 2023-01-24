b5eaf214e63ecc34cc5ed5c5337b13ec664d74e5
Crspy2 <81105520+Crspy2@users.noreply.github.com>
2023-01-23 22:08:43 -0800

    Added scroll for more

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { Incentive, Footer, Header, Nav, ProductCard } from '../components'
 import { client } from '../lib/sanity.client'
 import { motion } from 'framer-motion'
 
-import { BsFillArrowUpCircleFill } from 'react-icons/bs'
+import { BsFillArrowUpCircleFill, BsMouse } from 'react-icons/bs'
 import HideOn from '../Crspy-Scroll-API/Components/HideOn';
 import HideBetween from '../Crspy-Scroll-API/Components/HideBetween';
 
@@ -24,48 +24,63 @@ const Home = ({bannerData, products, incentiveData }: Props) => {
 
   return (
     <div className="bg-[#0F0E13]">
-        <div className="gradient-bg h-full">
-          <div className='pb-20'>
-            <Nav />
-          </div>
-          <Header bannerInfo={bannerData}/>
-        </div> 
-        
-        <section id="about" className='-mt-20 pt-20'>
-          <Incentive incentiveInfo={incentiveData} />
-        </section>
-        
-        <section id='products' className="products-container py-20">
-          <motion.h2 
-            initial={{ opacity: 0, y: 200, scale: 0.5 }}
-            viewport={{ once: true }}
-            whileInView={{ opacity: 1, y: 0, scale: 1 }}
-            transition={{ duration: 0.5 }}
-            className="text-5xl text-center font-extrabold tracking-tight py-10 text-transparent bg-clip-text bg-gradient-to-r from-[#007CF0] to-[#00DFD8] cursor-default"
-          >
-            Our Products
-          </motion.h2>
-          <motion.div 
-            initial={{ opacity: 0, y: 200, scale: 0.5 }}
-            viewport={{ once: true }}
-            whileInView={{ opacity: 1, y: 0, scale: 1 }}
-            transition={{ duration: 0.5 }}
-            className='products-container'
-          >
-            {products?.map((product) => product.name.includes("R6 Legit") ? <ProductCard key={product._id} productInfo={product} /> : null)}
-          </motion.div>
-          <motion.div 
-            initial={{ opacity: 0, y: 200, scale: 0.5 }}
-            viewport={{ once: true }}
-            whileInView={{ opacity: 1, y: 0, scale: 1 }}
-            transition={{ duration: 0.5 }}
-            className='products-container'
-          >
-          {products?.map((product) => product.name.includes("R6 Rage") ? <ProductCard key={product._id} productInfo={product} /> : null)}
-          </motion.div>
-        </section>
+      <div className="gradient-bg h-full">
+        <div className='pb-20'>
+          <Nav />
+        </div>
+        <Header bannerInfo={bannerData}/>
+      </div> 
+      
+      <section id="about" className='-mt-20 pt-20'>
+        <Incentive incentiveInfo={incentiveData} />
+      </section>
+      
+      <section id='products' className="products-container py-20">
+        <motion.h2 
+          initial={{ opacity: 0, y: 200, scale: 0.5 }}
+          viewport={{ once: true }}
+          whileInView={{ opacity: 1, y: 0, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="text-5xl text-center font-extrabold tracking-tight py-10 text-transparent bg-clip-text bg-gradient-to-r from-[#007CF0] to-[#00DFD8] cursor-default"
+        >
+          Our Products
+        </motion.h2>
+        <motion.div 
+          initial={{ opacity: 0, y: 200, scale: 0.5 }}
+          viewport={{ once: true }}
+          whileInView={{ opacity: 1, y: 0, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className='products-container'
+        >
+          {products?.map((product) => product.name.includes("R6 Legit") ? <ProductCard key={product._id} productInfo={product} /> : null)}
+        </motion.div>
+        <motion.div 
+          initial={{ opacity: 0, y: 200, scale: 0.5 }}
+          viewport={{ once: true }}
+          whileInView={{ opacity: 1, y: 0, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className='products-container'
+        >
+        {products?.map((product) => product.name.includes("R6 Rage") ? <ProductCard key={product._id} productInfo={product} /> : null)}
+        </motion.div>
+      </section>
 
-      <HideBetween height={true} startHeight={500} endHeight={2100} inverse={true}>
+      <HideOn height={20}>
+          <div className="sticky bottom-10 pl-20 w-full cursor-pointer">
+            <div className="flex items-start justify-start animate-bounce">
+            <svg  version="1.1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="17" height="26" viewBox="0, 0, 17, 26" 
+                  className="h-6 w-6 rounded-full cursor-pointer mr-3 fill-gay-400">
+              <g id="Page-1">
+                <path d="M8.5,5.5 L8.5,5.5 C8.776,5.5 9,5.724 9,6 L9,8 C9,8.276 8.776,8.5 8.5,8.5 L8.5,8.5 C8.224,8.5 8,8.276 8,8 L8,6 C8,5.724 8.224,5.5 8.5,5.5 z" fill="#FFFFFF" id="Rectangle-11"/>
+                <path d="M8.5,0 C13.194,0 17,3.806 17,8.5 L17,17.5 C17,22.194 13.194,26 8.5,26 C3.806,26 0,22.194 0,17.5 L0,8.5 C0,3.806 3.806,0 8.5,0 z M8.5,1 C4.358,1 1,4.358 1,8.5 L1,17.5 C1,21.642 4.358,25 8.5,25 C12.642,25 16,21.642 16,17.5 L16,8.5 C16,4.358 12.642,1 8.5,1 z" fill="#FFFFFF"/>
+              </g>
+            </svg>
+              <p className="text-gray-400 text-[14px] font-light">Scroll for more</p>
+            </div>
+          </div>
+      </HideOn>
+
+      <HideBetween height={true} startHeight={450} endHeight={2100} inverse={true}>
         <Link href="/">
           <div className="sticky bottom-5 w-full cursor-pointer">
             <div className="flex items-end justify-end pr-10">
